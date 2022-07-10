18d7de2c1447a7f45913456d6a72107ad6aff510
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-07-10 10:51:51 -0300

    Header 90%

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+const tabItems = document.querySelectorAll('.nav-option'); 
+const tabItemsContent = document.querySelectorAll('.tab-content-item')
+
+function selectItem(e){
+    removeShow(); 
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add('show');
+}
+
+function removeShow(){
+    tabItemsContent.forEach(item => {
+        item.classList.remove('show');
+    })
+}
+
+
+tabItems.forEach(item => {
+    item.addEventListener('mouseover', selectItem);
+})
+
+
+tabItems.forEach(item => {
+    item.addEventListener('click', removeShow);
+})
+
