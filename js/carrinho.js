f7e16fbf39f5fb54de60946219f5ecba1ec729d9
João Vitor de Abreu Sousa <jvabreusousa14@gmail.com>
2022-07-31 10:34:01 -0300

    Funcionalidade nav

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,4 +1,4 @@
-const btnAbrir = document.querySelector('.carrinho-btn-abrirTest'); 
+const btnAbrir = document.querySelector('.carrinho-btn');
 const btnFechar = document.querySelector('.carrinho-btn-fechar'); 
 const asideLateral = document.querySelector('.carrinho-lateral'); 
 
@@ -8,6 +8,7 @@ const asideLateral = document.querySelector('.carrinho-lateral');
 btnAbrir.addEventListener('click', () => {
     asideLateral.classList.add('carrrinho-lateral--ativo');
 })
+
 
 
 btnFechar.addEventListener('click', () => {
